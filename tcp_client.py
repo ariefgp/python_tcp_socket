17e0e6599afbc5ed9a8624913f0addcfe97a054c
ariefgp <arief.gusti@gmail.com>
2019-03-30 14:49:57 +0700

    add tcp for client

--- tcp_client.py
+++ tcp_client.py
@@ -2,7 +2,7 @@
 import socket
 
 # definisikan tujuan IP server
-HOST = '127.0.0.1' 
+HOST = '127.0.0.1'
 
 # definisikan port dari server yang akan terhubung
 PORT = 65432 
@@ -11,29 +11,23 @@ PORT = 65432
 buffer = 7
 
 # definisikan pesan yang akan disampaikan
-pesan = "Contoh Pesan"
+pesan = "received pesan"
 
 # buat socket TCP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # lakukan koneksi ke server dengan parameter IP dan Port yang telah didefinisikan
-	s.bind((HOST, PORT))
-	s.listen(buffer)
+s.connect((HOST, PORT))
 
 # kirim pesan ke server
-	conn, addr = s.accept()
-    with conn:
+s.send("I am CLIENT\n")
 
 # terima pesan dari server
-		print('Connected by', addr)
+from_server = s.recv(4096)
 
 # tampilkan pesan/reply dari server
-		while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+print from_server
 
 # tutup koneksi
-	conn.close()
-    print 'client disconnected'
+s.close()
+
